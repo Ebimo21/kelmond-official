9710e88973cb1a8c4fce5613eeb3e8b9ddf54f1b
ebimo <xebi4jah15@gmail.com>
2020-09-17 11:13:30 +0100

    almost there

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
+var nav_state = "close";
 
-function open(){
-    alert('hi');
-    // document.getElementById('card1').style.display = "none";
+function open_menu(){
+
+    if(nav_state == "close"){
+    console.log("except");
+    document.getElementById('menu').style.height = "100%";
+
+    nav_state = "open"
+    }
+    else{
+        document.getElementById('menu').style.height = "0%";
+        nav_state = "close";
+    }
+
+
 }
